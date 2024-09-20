f4d47e2ddf679325b090f7a25ebc6942347c701f
QHWU1228 <qwu22@binghamton.edu>
2024-09-20 00:11:22 -0400

    updating test cases for the tester

--- TesterMain.java
+++ TesterMain.java
@@ -5,6 +5,20 @@ public class TesterMain {
         // ArrayListQueue2<Integer> queue21 = null; 
         // queue21.enqueue(69);
         // enqueue does not catch a null arraylist2 object
+        System.out.println("##########Test for ArrayListQueue1#############");
+        ArrayListQueue1<Integer> alq1 = new ArrayListQueue1<>();
+        System.out.println("enqueue 69"); alq1.enqueue(69);
+        System.out.println("Did it dequeue? " + alq1.dequeue());
+        // System.out.println("Did it dequeue? " + queue21.dequeue()); 
+        // dequeue does not catch a null or empty queue
+        System.out.println("enqueue 69"); alq1.enqueue(69);
+        System.out.println("Did it removeQueue 69? " + alq1.removeQueue(69));
+        System.out.println("Did it removeQueue 69? " + alq1.removeQueue(69));
+        System.out.println("Is queue empty? " + alq1.isEmpty());
+        System.out.println("enqueue 69"); alq1.enqueue(69);
+        System.out.println("Is queue empty? " + alq1.isEmpty());
+        System.out.println("###############################################");
+
         System.out.println("##########Test for ArrayListQueue2#############");
         ArrayListQueue2<Integer> queue21 = new ArrayListQueue2<>();
         System.out.println("enqueue 69"); queue21.enqueue(69);
