404cd66c905ede1791aff78522120dc222113158
Xinwei Guo <xguo50@binghamton.edu>
2024-09-19 23:48:52 -0400

    error comments

--- TesterMain.java
+++ TesterMain.java
@@ -2,13 +2,15 @@ package CS_442_Assignment2;
 
 public class TesterMain {
     public static void main(String[] args) {
-        // ArrayListQueue2<Integer> queue21 = null;
+        // ArrayListQueue2<Integer> queue21 = null; 
         // queue21.enqueue(69);
+        // enqueue does not catch a null arraylist2 object
         System.out.println("##########Test for ArrayListQueue2#############");
         ArrayListQueue2<Integer> queue21 = new ArrayListQueue2<>();
         System.out.println("enqueue 69"); queue21.enqueue(69);
         System.out.println("Did it dequeue? " + queue21.dequeue());
-        //System.out.println("Did it dequeue? " + queue21.dequeue());
+        // System.out.println("Did it dequeue? " + queue21.dequeue()); 
+        // dequeue does not catch a null or empty queue
         System.out.println("enqueue 69"); queue21.enqueue(69);
         System.out.println("Did it removeQueue 69? " + queue21.removeQueue(69));
         System.out.println("Did it removeQueue 69? " + queue21.removeQueue(69));
